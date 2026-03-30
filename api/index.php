3bdcace4a1954c912a56c625f4b9a39241c8b606
alvira41 <alviraoct08@gmail.com>
2026-03-30 10:45:12 +0700

    Fix: Redirect cache to /tmp and clean bootstrap

--- api/index.php
+++ api/index.php
@@ -1,5 +1,8 @@
 <?php
+// Gunakan autoload dari vendor
 require __DIR__ . '/../vendor/autoload.php';
+
+// Muat aplikasi Laravel
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
